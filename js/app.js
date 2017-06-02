05a12bb4d2a4f4bf16b30361be0ef05659ae449b
PabloCasvar <casvar.pablo@gmail.com>
2017-06-02 01:01:17 -0500

    Inicio de proyecto

---
+++ js/app.js
@@ -0,0 +1,71 @@
+(function(angular){
+
+var app = angular.module("myApp", []);
+
+app.component('plotSvg',{
+    bindings:{
+        width: "@",
+        height: "@",
+        minX: "@",
+        maxX: "@",
+        minY: "@",
+        maxY: "@"
+    },
+    controllerAs: 'ctrl',
+    controller: function($scope){
+        this.$onInit = function(){
+           //set default values
+           if(!this.width) this.width = 100;
+           if(!this.height) this.height = 100;
+           if(!this.minX) this.minX = -5;
+           if(!this.maxX) this.maxX = 5;
+           if(!this.minY) this.minY = -5;
+           if(!this.maxY) this.maxY = 5;
+           
+           this.setDeltaX();
+           this.setDeltaY();
+           this.setOriginX();
+           this.setOriginY();
+           console.log(this.originY);
+           console.log(this.dy);
+        };
+
+        this.setDeltaX = function(){
+            this.dx = this.width/(this.maxX-this.minX);
+        };
+        this.setDeltaY = function(){
+            this.dy = this.height/(this.maxY-this.minY);
+        };
+        this.setOriginX = function(){
+            var min = this.minX;
+            var max = this.maxX;
+            var origin;
+            if(min>=0){ 
+                origin=-1*min*this.dx;
+            }else if(min<0 && max>0){
+                origin = -1*min*this.dx;
+            }else if(max <= 0){
+                origin = this.width-max*this.dx;
+            }
+            this.originX = origin;
+        };
+        this.setOriginY = function(){
+            var min = this.minY;
+            var max = this.maxY;
+            var origin;
+            if(min>=0){ 
+                origin=-1*min*this.dy;
+            }else if(min<0 && max>0){
+                origin = -1*min*this.dy;
+            }else if(max <= 0){
+                origin = this.height-max*this.dy;
+            }
+            this.originY = origin;
+        };
+    }, 
+    templateUrl: "templates/svg.tpl.html"
+});
+
+})(window.angular);
+
+
